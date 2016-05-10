a41e525e9c4fc6428400ea20ca573777fe3dae8a
tonderai ndangana <ndangana8@gmail.com>
2016-05-10 22:28:33 +0200

    5th commit

--- tpg/src/main/java/zw/co/hitrac/tpg/web/page/HomePage.java
+++ tpg/src/main/java/zw/co/hitrac/tpg/web/page/HomePage.java
@@ -6,11 +6,21 @@
 package zw.co.hitrac.tpg.web.page;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
  *
  * @author hitrac
  */
 public class HomePage extends WebPage {
+    
+      public HomePage(PageParameters parameters) {
+        super(parameters);
+        add(new BookmarkablePageLink("level", LevelListPage.class));
+        
+        
+       
+    }
     
 }
